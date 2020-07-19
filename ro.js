f4bce9646f1ecc73be6cdbad77ed7259943421e2
Abdullahbaki <35779784+Abdullahbaki@users.noreply.github.com>
2020-07-19 14:24:58 +0300

    Ro

---
+++ ro.js
@@ -0,0 +1,149 @@
+"i": Faceți click aici pentru a adăuga acest robot la serverul dumneavoastră.
+"i_dm": Verifică mesajele primite.
+"i_ss": roboții sprijină serverul
+"q_c": subiectul mesajului
+"q_l": link-ul mesajului
+"q_l_c": poți să lași mesajul aici
+"q_m_o": mesajul proprietarului
+"q": mesajul citat
+"q_m_err": te rugăm să adaug un ID valid
+"q_m_err2": ID-ul trebuie să conțină doar numere. Te rugăm să adaug un ID valid
+"um":Mesajul nu poate fi găsit
+"prefix":Prefix/prefixe
+"undefined":Datele nu se găsesc.
+"help_title":Ajutor
+"help_desc":Arată comenzile 
+"command_owner_only": Această comandă este doar pentru proprietar
+"not_enough_permission": Ai nevoie de permisiune pentru a utiliza această comandă.
+command_guild_only": Această comandă se poate folosi doar în serverul suport
+"ping_command": Semnalul tău este:
+"specify_language": Te rugăm să specifici o limbă
+"specify_valid_language": Te rugăm să adaugi o limbă validă. Limbi valide:
+"language_updated": Limba ta a fost încărcată cu succes.
+"warn":Avertisment
+"succ":Succes
+"result":Rezultat
+"auth":Persoana autorizată
+"action":Acțiune
+"chat":Convorbire
+"footer":Lansat de smskBOT 
+"p_by":Șters de
+"p_c":Câți?
+"p_purge":Ștergerea convorbirii
+"p_perm_err":Ai nevoie de permisiunea managerului de a șterge mesajele
+"p_c_err":Te rugăm să introduci un număr valid
+"p_c_100_err":Te rugăm să introduci un număr mai mic ca 100
+"ann":Anunț
+"ann_sys":smskBOT anunț sistem
+"ann_args_err":Ar trebui să scrieți undeva pentru a anunța
+"avatar_sys":Sistemul avatarului 
+"avatar_show":Arată avatarul mai mare
+"avatar_err":A apărut o eroare
+"u":Actualizare
+"u_title":O noua actualizare a fost făcută
+"u_info":Informațiile actualizării
+"v_arg_err":Trebuie să scri ceva ca să votezi
+"v_info":Pentru a vota cu "Da":thumbsup:Pentru a vota cu "Nu":thumbsdown.
+"v":Votează
+"g":Dăruiește 
+"g_args_err":Trebuie să scrieți ce va fi tragerea la sorți
+"g_prize":Premiu
+"g_winner":Câștigător
+"e":Utilizare: componente [Titlu];[Descriere];[Culoare];[Imagine]
+"e_err":Eroare!Trebuie sa specifici titlul!
+"banned":Interzis!
+"ban_err":Eroare de autoritate! Utilizatorii au avut fie mai multă autoritate decât mine, fie există o eroare de cod. Vă rugăm să raportați eroarea!
+"k_dm":Nu poți să folosești aceasta comandă la mesaje.
+"k_mention":Menționează persoana te rog
+"k_admin_err":Nu poți lovi adminii
+"kkicked":Lovit!!!
+"north":Nord
+"north_east": Nord_Est
+"northeast":Nord Est
+"east":Est
+"east_south":Est_Sud
+"southeast":Sud Est
+"south_east":Sud_Est 
+"south":Sud
+"south_southwest":Sud Sud_Vest
+"south_west":Sud Vest
+"west_south":Vest_Sud
+"west":Vest
+"west_nord":Vest_Nord
+"northwest":Nord-Est
+"north_west":Nord_Nord-Vest
+"w_info":Oprire aer pentru
+"w_coor":Coordonate
+"w_lat":Latitudine
+"w_long":Longitudine
+"w_c_id":Obțineți licența
+"w_wind":Vânt
+"w_direction":Direcție
+"w_speed":Viteză
+"w_could":Viteză de nori
+"w_cond":Brăzdare de aer
+"w_curr_temp":Căldura
+"w_high_temp":Temperatura maximă
+"w_low_temp":Temperatură minimă
+"w_mois":Umiditate
+"ww_bar_press":Presiunea barometrică 
+"w_sun":Soarele 
+"w_sunrise":Răsărit
+"w_sunset":Apus
+"c_question":Întrebare 
+"c_answer":Răspuns
+"c_err":Te rugăm să specifici o acțiune.
+"c_m_err":Actiune greșită
+"modlog_err":Te rugăm să specifici un canal identificator
+"modlog_updated": Canal actualizat cu succes
+"j_title":Glumă
+"t_l_err":Specifică o limbă pentru traducere
+"t_m_err":Te rugam specifică un cuvânt sau o propoziție de tradus
+"t":Traduce
+"t_text":Text 
+"translate": Traducerile nu pot fi complete.Te rrugăm să adaugi serverul nostru suport pentru a corecta această problemă".
+"us_central":America
+"us_east":America centrală
+"us_south":America de Sud
+"us_west":America de Vest
+"eu_west":Europa de Vest
+"singapore":Singapore
+"london":Londra
+"japan":Japonia
+"russia":Rusia
+"hongkong":Hong Kong
+"brazil":Brazilia
+"sydney":Sidney
+"southafrica":Africa de sud
+"s_info":Informațiile serverului
+"s_creator":Serverul creatorului
+"s_id":ID-ul serverului
+"s_created_at":Creat la
+"s_region":Serverul regiunii 
+"s_channels":Canale
+"s_users":Utilizatori
+"s_afk_timeout":Pauză
+"s_emoji":Număr emoji
+"s_role":Număr rol
+"text":Text 
+"voice":Voce
+"category":Categorie
+"s_afk_undefined":Nu se poate găsi canalul
+"online":Online
+"offline":Offline
+"dnd":Nu deranja
+"Bot":Robot
+"sec":Secunde
+"s_emoji_info":Poți sa vezi toate emoji-urile prin accesarea serverului emoji
+"s_role_info": Poti sa vezi toate rolurile în serverul rol
+"err_args":Te rugam introdu Args 
+"g_succ":Aici este link-ul tău
+"day":Zile
+"hour":Ore
+"min":Minute
+"m_id":ID-ul membrului
+"m_roles":Roluri
+"m_created_at":Contul creat
+"m_joined":Conectat la server
+"m_stats":Status
+"days_ago":Zile în urmă.
